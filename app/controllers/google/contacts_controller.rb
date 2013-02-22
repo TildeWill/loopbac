1af79b960c24f467ccb67d286f52d3f1a9830b3f
Will Read <will.read@gmail.com>
2013-02-21 22:14:38 -0800

    store the new token if it is refreshed

--- app/controllers/google/contacts_controller.rb
+++ app/controllers/google/contacts_controller.rb
@@ -2,7 +2,12 @@ module Google
   class ContactsController < ApplicationController
     respond_to :json, :html
     def index
-      contacts = Google::Contact.get_all(current_user.domain, current_user.oauth2_token, current_user.refresh_token)
+      contacts = Google::Contact.get_all(
+          current_user.domain,
+          current_user.oauth2_token,
+          current_user.refresh_token) do |new_token|
+        current_user.update_attributes(:oauth2_token => new_token)
+      end
       @contacts = Google::ContactDecorator.decorate_collection(contacts)
     end
   end
--- app/models/google/contact.rb
+++ app/models/google/contact.rb
@@ -1,7 +1,12 @@
 module Google
   class Contact
-    def self.get_all(domain, token, refresh_token)
-      client = GoogleAppsOauth2::Transport.new(domain: domain, token: token, client_id: GOOGLE_APP_ID, client_secret: GOOGLE_APP_SECRET, refresh_token: refresh_token)
+    def self.get_all(domain, token, refresh_token, &block)
+      client = GoogleAppsOauth2::Transport.new(
+          domain: domain,
+          token: token,
+          client_id: GOOGLE_APP_ID,
+          client_secret: GOOGLE_APP_SECRET,
+          refresh_token: refresh_token, &block)
       client.get_users
     end
   end
