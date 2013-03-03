07213c34d93eb21bd4928ddb22189915f11488cc
Will Read <will.read@gmail.com>
2013-03-03 13:31:32 -0800

    don't need the file system store

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_apps, :store => OpenID::Store::Filesystem.new('./tmp')
+  provider :google_apps#, :store => OpenID::Store::Filesystem.new('./tmp')
 end
