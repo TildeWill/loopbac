aef4151d2c32b2705b7f026c859030172108a9c7
Will Read <will.read@gmail.com>
2013-03-02 21:29:34 -0800

    skip auth on calendar controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,6 @@
 class CalendarsController < ApplicationController
+  skip_before_filter :authenticate_user!
+  
   respond_to :json
   def show
     respond_with Google::Calendar.events('will@loopb.ac')
