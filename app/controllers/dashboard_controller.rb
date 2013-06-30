ecb27760fa35f7d74132570a241490e34242f85e
Ian Lesperance & Will Read <pair+ian+will@loopb.ac>
2013-06-30 13:12:35 -0700

    Make meta_feedback form work by using meta_feedback#new

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,4 @@
 class DashboardController < ApplicationController
   def index
-    #@feedback = Feedback.new
-    feedback_for_review = Feedback.can_review(current_user).first
-    @meta_feedback = MetaFeedback.new(feedback: feedback_for_review) if feedback_for_review
   end
 end
