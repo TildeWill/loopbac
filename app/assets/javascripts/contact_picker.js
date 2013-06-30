6b92f0cf8dd7f2e54adac01d71c1efcf134c3886
Ian Lesperance & Will Read <pair+ian+will@loopb.ac>
2013-06-30 13:08:10 -0700

    Make feedback form work by using feedback#new

--- app/assets/javascripts/contact_picker.js
+++ app/assets/javascripts/contact_picker.js
@@ -1,20 +1,22 @@
-$(function () {
-  $("input.contact_picker").autocomplete({
-    minLength: 0,
-    source: $("input.contact_picker").data('contacts'),
-    focus: function (event, ui) {
-      $(this).val(ui.item.label);
-      $(this).nextAll('input').val(ui.item.value);
-      return false;
-    },
-    select: function(event, ui) {
-      $(this).val(ui.item.label);
-      $(this).nextAll('input').val(ui.item.value);
-      return false;
-    }
-  }).data("ui-autocomplete")._renderItem = function (ul, item) {
-    return $("<li>")
+$.fn.extend({
+  contactPicker: function() {
+    this.autocomplete({
+      minLength: 0,
+      source: $("input.contact_picker").data('contacts'),
+      focus: function (event, ui) {
+        $(this).val(ui.item.label);
+        $(this).nextAll('input').val(ui.item.value);
+        return false;
+      },
+      select: function(event, ui) {
+        $(this).val(ui.item.label);
+        $(this).nextAll('input').val(ui.item.value);
+        return false;
+      }
+    }).data("ui-autocomplete")._renderItem = function(ul, item) {
+      return $("<li>")
         .append("<a><img class='profile-icon' src='" + item.icon + "'/>" + item.label + "</a>")
         .appendTo(ul);
-  };
+    };
+  }
 });
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @feedback = Feedback.new
+    #@feedback = Feedback.new
     feedback_for_review = Feedback.can_review(current_user).first
     @meta_feedback = MetaFeedback.new(feedback: feedback_for_review) if feedback_for_review
   end
